cebc2e3f22bb67bc6c4299a9e8710e561cab9a32
Evan Marie Carr <evanmariecarr@gmail.com>
2023-05-12 14:42:54 -0400

    refactoring code

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import apiClient, { GetResponse } from "../services/api-client";
+
+import platforms from "../data/platforms"
 
 export interface Platform {
     id: number;
@@ -6,6 +9,15 @@ export interface Platform {
     slug: string;
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+const usePlatforms = () => useQuery({
+    queryKey: ['platforms'],
+    queryFn: () => 
+    apiClient
+        .get<GetResponse<Platform>>('/platforms/lists/parents')
+        .then((response) => response.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    //initialData: { count: platforms.length, results: platforms }
+})
 
 export default usePlatforms;
+
