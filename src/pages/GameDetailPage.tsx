ae9f467c2342c7f744e65a605109a420217bd7c7
Evan Marie Carr <evanmariecarr@gmail.com>
2023-05-23 15:08:11 -0400

    fixed path problem

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -4,6 +4,7 @@ import { Box, GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from "../components/GameTrailer";
+// Path will not work without this ridicuous pathname
 import Screenshots from "../components/ScreenShots";
 
 const GameDetailPage = () => {
